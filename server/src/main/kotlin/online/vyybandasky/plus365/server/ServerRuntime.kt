3a444950550bbfc48d53c302aad1ba1374f146b0
bonniegachiengu <bonniegachiengu@gmail.com>
2026-09-22 08:27:17 +0300

    refactor: extract reusable server runtime (#12)

---
+++ server/src/main/kotlin/online/vyybandasky/plus365/server/ServerRuntime.kt
@@ -0,0 +1,64 @@
+package online.vyybandasky.plus365.server
+
+import java.io.File
+import online.vyybandasky.plus365.core.DevSeed
+import online.vyybandasky.plus365.core.book.LedgerBook
+import online.vyybandasky.plus365.core.store.encodeBook
+import online.vyybandasky.plus365.core.store.open
+
+class ServerRuntime(
+    val dataDir: File,
+    val port: Int = DEFAULT_PORT,
+    val host: String = DEFAULT_HOST,
+) {
+    private val ledgerFile = File(dataDir, "ledger.json")
+
+    private val store = ServerFileLedgerStore(ledgerFile)
+
+    private val opened = store.open {
+        DevSeed.book()
+    }
+
+    private val hub = SyncHub(
+        object : ServerLedger {
+            private var current: LedgerBook = opened.book
+
+            override fun read(): LedgerBook = current
+
+            override fun write(book: LedgerBook): Boolean =
+                runCatching {
+                    store.write(encodeBook(book))
+                    current = book
+                    true
+                }.getOrDefault(false)
+        },
+    )
+
+    val pairingCode: String = ServerPairingCode.loadOrCreate(dataDir)
+
+    val server = buildServer(
+        port = port,
+        host = host,
+        hub = hub,
+        pairingCode = pairingCode,
+    )
+
+    fun start() {
+        server.start(wait = false)
+    }
+
+    fun stop(
+        gracePeriodMillis: Long = 100,
+        timeoutMillis: Long = 1_000,
+    ) {
+        server.stop(gracePeriodMillis, timeoutMillis)
+    }
+
+    fun ledgerFile(): File = ledgerFile
+}
+
+fun defaultServerDataDir(): File =
+    File(
+        System.getProperty("user.home"),
+        ".365plus-server",
+    )
